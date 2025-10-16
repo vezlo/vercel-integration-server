bf435b80322bde8141e07dea368bc1e5de5c786f
mrehanali <syedmrehanali@gmail.com>
2025-10-16 12:05:43 +0500

    remove vercel.json and adjust configs for a clean deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverActions: {
-      enabled: true,
-    },
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
 
